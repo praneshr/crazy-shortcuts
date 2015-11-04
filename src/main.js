8e4935a6abe69d88dca1cf62bcbc951df1f32ef4
Pranesh Ravi <praneshr@indix.com>
2015-11-04 18:14:57 +0530

    Listing the values for selection

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
-function renderUI(data){
-  console.log(data);
+function renderUI(d){
+  var letters = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+{}|<>?:\"".split('');
+  var parsedData = JSON.parse(d.data);
+  var select = $('.select');
+  console.log(parsedData);
+  parsedData.map(function(obj,i){
+    var index = letters.indexOf(obj.key.toLowerCase())
+    if(index !== -1){
+      letters.splice(index, 1);
+    }
+  });
+  letters.map(function(v,i){
+    select.append('<option value="'+v+'">'+v.toUpperCase()+'</option>');
+  })
 }
 chrome.runtime.sendMessage({getKeyMaps: true},function(response){
   renderUI(response.data);
