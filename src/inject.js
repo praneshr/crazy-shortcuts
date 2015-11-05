a2d1ae9c21956fe9f47a90f16139c33ac67e2bb9
Pranesh Ravi <praneshr@indix.com>
2015-11-05 09:46:06 +0530

    Fixing event triggered during input focus

--- src/inject.js
+++ src/inject.js
@@ -1,6 +1,6 @@
 
 $(window).keypress(function(e){
-  if(e.shiftKey){
+  if(e.shiftKey && !$('input').is(':focus')){
     chrome.runtime.sendMessage({getKeyCode: String.fromCharCode(e.which),getKeyMap: false}, function(response) {
       if(response.url)
         window.open(response.url);
